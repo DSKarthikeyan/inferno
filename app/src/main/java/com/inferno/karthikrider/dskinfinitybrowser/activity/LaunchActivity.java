839b40303530d925a2ed7eabb166d2f7c2aa7d74
Karthikeyan D S <karthikrider92@gmail.com>
2018-11-07 15:52:23 +0530

    Initial Commit

---
+++ app/src/main/java/com/inferno/karthikrider/dskinfinitybrowser/activity/LaunchActivity.java
@@ -0,0 +1,20 @@
+package com.inferno.karthikrider.dskinfinitybrowser.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.inferno.karthikrider.dskinfinitybrowser.R;
+
+/**
+ * Created by karthikrider on 06-Nov-18.
+ */
+
+public class LaunchActivity extends Activity{
+
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.launch_activity);
+    }
+}
